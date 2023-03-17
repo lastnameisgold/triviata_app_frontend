d0eec83efadfd272b1a42cea4315020b4e82c320
juzorey <josejosereyes75@gmail.com>
2023-03-17 01:43:18 -0400

    added all comps empty files

---
+++ triviata/src/App.js
@@ -0,0 +1,13 @@
+import{ React } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      
+    </div>
+  );
+}
+
+export default App;
