0c90749343e504f7c17d19f04c7f4f22a580bb73
juzorey <josejosereyes75@gmail.com>
2023-03-19 10:15:44 -0600

    progress in creatingFlashCards

--- triviata/src/App.js
+++ triviata/src/App.js
@@ -1,14 +1,20 @@
 import { React } from 'react';
 import  Create  from './components/create/Create.jsx'
-import  CreateFlashcards  from './components/flashcards/CreateFlashcards.jsx'
+import  Flashcards  from './components/flashcards/Flashcards.jsx'
+import CreateTitle from './components/create/create_quiz/CreateTitle.jsx';
+import CreateDescription from './components/create/create_quiz/CreateDescription.jsx';
+import CreateFlashCards from './components/flashcards/CreateFlashcards.jsx';
+
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="app">
-      <CreateFlashcards/>
+      {/* <Flashcards/>
       <Create/>
+      <CreateDescription/> */}
+      <CreateFlashCards/>
     </div>
   );
 }
--- triviata/src/components/flashcards/CreateFlashcards.jsx
+++ triviata/src/components/flashcards/CreateFlashcards.jsx
@@ -1,49 +1,29 @@
-import { useState } from "react"
-export default function CreateFlashcards (){
-
-
-const[hidden,setHidden] = useState('false')
-const showDef = ()=>{
-setHidden(!hidden)
-}
-
-
+import CreateDescription from "../create/create_quiz/CreateDescription"
+import CreateTitle from "../create/create_quiz/CreateTitle"
+export default function CreateFlashCards(){
   return(
-  <div className="flashcards-container" onClick={showDef}>
-    <div className="flashcards">
+    <div className="create-flashcards-container"> 
+      <CreateTitle/>
+      <CreateDescription/>
+      <div className="create-tiles-container">
+        <div className="tiles-container">
+          <div className="tiles-inner-container">
+            <div className="term-def-container">
+              <input className="term-container">
 
-      <div className="flashcards-term" hidden={hidden? true:false}>
-          <div className="flashcards-inner-container">
-            <div className="flashcard-number"><span>1/24</span></div>{/* change with state*/}
+              </input>
+              <input className="def-container">
 
-            <div className="term-container"> {/*this can have a state to change the classname to display either term or definiotion based on click*/}
-              <div className="term"><span>Cow Color</span></div>
+              </input>
             </div>
 
-            <div className="prev-next-container">
-              <div className="prev-card"><span>←</span></div>
-              <div className="next-card"><span>→</span></div>
-            </div>
-          </div>
-      </div>
-
-
-
-      <div className="flashcards-definition" hidden={!hidden? true:false}> {/*this changes from hidden true to false and the classname changes to show on click it will flip*/}
-        <div className="flashcards-inner-container">
-          <div className="flashcard-number"><span>1/24</span></div>{/* change with state*/}
-          
-          <div className="term-container"> {/*this can have a state to change the classname to display either term or definiotion based on click*/}
-            <div className="term"><span>Color of cow is white and black</span></div>
           </div>
 
-          <div className="prev-next-container">
-            <div className="prev-card"><span>←</span></div>
-            <div className="next-card"><span>→</span></div>
-          </div>
         </div>
+
       </div>
+
     </div>
-  </div>
   )
+  
 }
