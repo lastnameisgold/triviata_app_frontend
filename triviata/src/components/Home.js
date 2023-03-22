b32c2d71e8e84ec27c3cc377a6e267c29878c5b3
Fernando <me@fdorado.com>
2023-03-22 17:23:28 -0400

    Added ternary for authenticated vs unauthenticated home page views.

--- triviata/src/components/Home.js
+++ triviata/src/components/Home.js
@@ -1,7 +1,12 @@
 import Card from "./Card"
+import { useNavigate } from 'react-router-dom'
+import { useEffect, useState } from "react"
 
-export default function Home() {
-    return(
+export default function Home({ user, authenticated }) {
+    // let navigate = useNavigate()
+    // const [quizzes, setQuizzes] = useState([])
+
+    return(user && authenticated) ? (
         <div className="home-container">
             <div className="section-container">
                 <h3>Trending</h3>
@@ -34,6 +39,34 @@ export default function Home() {
                     <Card />
                     <Card />
                     <Card />
+                </div>
+            </div>
+        </div>
+    ) : (
+        <div className="home-signedout-container">
+
+            <div className="hero-container">
+                <div className="hero-content-container">
+                    <h1>Ignite Your Curiosity: Discover, Learn, and Quiz!</h1>
+                    <p>Discover a world of knowledge, ignite your curiosity, and enhance your learning journey with engaging quizzes and interactive flashcards.</p>
+                </div>
+                <button className="primary-button">Sign up for free</button>
+            </div>
+            <div className="features-section">
+                <div className="feature-section">
+                    <img src="https://images.unsplash.com/photo-1573495783323-9e59a325d4b4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=988&q=80" alt="" />
+                    <div className="feature-content">
+                        <h3>Lorem ipsum dolor sit amet consectetur. Semper lectus morbi.</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur. Aliquet turpis in a nullam et. In ultrices risus pellentesque sapien turpis velit consectetur tempus.</p>
+                    </div>
+                </div>
+
+                <div className="feature-section">
+                    <div className="feature-content">
+                        <h3>Lorem ipsum dolor sit amet consectetur. Semper lectus morbi.</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur. Aliquet turpis in a nullam et. In ultrices risus pellentesque sapien turpis velit consectetur tempus.</p>
+                    </div>
+                    <img src="https://images.unsplash.com/photo-1618557039181-a3b7e72beeda?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=988&q=80" alt="" />
                 </div>
             </div>
         </div>
--- triviata/src/components/Nav.js
+++ triviata/src/components/Nav.js
@@ -1,12 +1,33 @@
 import { Link } from "react-router-dom"
 
-export default function Nav() {
+export default function Nav({authenticated, user, handleLogOut}) {
+    let authenticatedOptions
+    if (user) {
+        authenticatedOptions=(
+            <nav>
+            <h3>Welcome {user.email}!</h3>
+            <Link to="/feed">Feed</Link>
+            <Link onClick={handleLogOut} to="/">
+              Sign Out
+            </Link>
+          </nav>
+        )
+    }
+
+    const publicOptions = (
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/register">Register</Link>
+          <Link to="/signin">Sign In</Link>
+        </nav>
+      )
+
     return(
         <div className="nav-container">
             <Link to="/">
                 <h1>Triviata</h1>
             </Link>
-            
+
             <div className="search-bar">
                 <span className="material-symbols-rounded">search</span>
                 <input className="search-input" placeholder="Search for quizzes, flashcards, topics"/>
@@ -27,15 +48,14 @@ export default function Nav() {
                 </Link>
                 
                 {/* For testing purposes! Feel free to change link design for register */}
-                
+
                 <Link to="/signin">
                     <h3>Sign In</h3>
                 </Link>
                 <Link to="/register">
-                    <h3>Sign Up</h3>
+                    <h3>Register</h3>
                 </Link>
-                
-
+                {authenticated && user ? authenticatedOptions : publicOptions}
             </div>
         </div>
     )
